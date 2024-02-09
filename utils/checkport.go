cb5a08ccbd28445e06618190aed2020049054d2e
Mehmet Eraslan <mehmeterslan@gmail.com>
2024-02-09 14:44:36 +0300

    get hostname, get ip adress and port check func added

---
+++ utils/checkport.go
@@ -0,0 +1,48 @@
+package utils
+
+import (
+	"net"
+	"os"
+	"path/filepath"
+	"strconv"
+	"time"
+
+	"github.com/Netmera/prometheus-consul-exporter/models"
+	yaml "gopkg.in/yaml.v2"
+)
+
+// LoadConfigFromFile reads configuration from a YAML file
+func LoadConfigFromFile(filename string) (*models.CheckPortModel, error) {
+	// Dosya yolunu bul
+	filepath, err := filepath.Abs(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	// Dosyayı aç
+	file, err := os.Open(filepath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	// Yapılandırma verilerini parse et
+	var config models.CheckPortModel
+	decoder := yaml.NewDecoder(file)
+	err = decoder.Decode(&config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
+
+func CheckPortOpen(port int) bool {
+	address := net.JoinHostPort("localhost", strconv.Itoa(port))
+	conn, err := net.DialTimeout("tcp", address, time.Second)
+	if err != nil {
+		return false
+	}
+	defer conn.Close()
+	return true
+}
